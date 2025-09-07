f3f2fad19e09ea8f150ba592a9e642a39848f850
Niamh <niamh.elizabeth@mail.com>
2025-09-07 23:01:02 +0100

    Added detect_options.py: working product option dectection

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from playwright.sync_api import sync_playwright
 
+PRODUCT_URL = 'https://www.popmart.com/gb/products/1064/THE-MONSTERS-Big-into-Energy-Series-Vinyl-Plush-Pendant-Blind-Box'
+
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto('https://www.popmart.com/gb/products/1064/THE-MONSTERS-Big-into-Energy-Series-Vinyl-Plush-Pendant-Blind-Box')
+        page.goto(PRODUCT_URL)
         print('Page title:', page.title())
         page.pause() # Pause to allow user interaction
         browser.close()
